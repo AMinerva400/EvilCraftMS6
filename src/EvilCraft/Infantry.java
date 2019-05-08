129ff8c0b61efa3fc86bddf5a2b0d8ae9af30070
JauharAkash <akashjauhar18@gmail.com>
2019-05-08 13:32:19 -0400

    Infantry.java

--- src/EvilCraft/Infantry.java
+++ src/EvilCraft/Infantry.java
@@ -16,7 +16,7 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 package EvilCraft;
-
+//merge conflict fixed by Akash
 import BridgePattern.ICanvasDevice;
 
 /**
